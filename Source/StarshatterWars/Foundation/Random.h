99e37110407012629721c7e090181edb87df448a
Starshatter Wars <carlos.bott@gmail.com>
2024-11-07 11:41:07 +0000

    Utility functions

---
+++ Source/StarshatterWars/Foundation/Random.h
@@ -0,0 +1,36 @@
+/*  Project Starshatter Wars
+	Fractal Dev Games
+	Copyright (C) 2024. All Rights Reserved.
+
+	SUBSYSTEM:    Foundation
+	FILE:         RLoc.h
+	AUTHOR:       Carlos Bott
+
+	OVERVIEW
+	========
+	Utility functions for generating random numbers and locations.
+*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Types.h"
+#include "Geometry.h"
+
+/**
+ * 
+ */
+// +----------------------------------------------------------------------+
+
+	void     RandomInit();
+	Point    RandomDirection();
+	Point    RandomPoint();
+	Vec3     RandomVector(double radius);
+	double   Random(double min = 0, double max = 1);
+	int      RandomIndex();
+	bool     RandomChance(int wins = 1, int tries = 2);
+	int      RandomSequence(int current, int range);
+	int      RandomShuffle(int count);
+
+	// +----------------------------------------------------------------------+
+
