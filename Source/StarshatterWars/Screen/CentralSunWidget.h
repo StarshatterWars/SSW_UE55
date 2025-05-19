4d5f94acc55f4ae611dba80ccf4dfc3db1e20b88
Carlos Bott <carlos.bott@gmail.com>
2025-05-19 22:49:56 +0800

    adding planet markers

---
+++ Source/StarshatterWars/Screen/CentralSunWidget.h
@@ -0,0 +1,20 @@
+// /*  Project nGenEx	Fractal Dev Games	Copyright (C) 2024. All Rights Reserved.	SUBSYSTEM:    SSW	FILE:         Game.cpp	AUTHOR:       Carlos Bott*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CentralSunWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARSHATTERWARS_API UCentralSunWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
