923bceb8d117574208713be6d1e0ad5e33acaeae
Carlos Bott <carlos.bott@gmail.com>
2025-05-26 18:55:18 +0800

    starting to add moons

---
+++ Source/StarshatterWars/Actors/MoonPanelActor.cpp
@@ -0,0 +1,28 @@
+// /*  Project nGenEx	Fractal Dev Games	Copyright (C) 2024. All Rights Reserved.	SUBSYSTEM:    SSW	FILE:         Game.cpp	AUTHOR:       Carlos Bott*/
+
+
+#include "MoonPanelActor.h"
+
+
+// Sets default values
+AMoonPanelActor::AMoonPanelActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMoonPanelActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMoonPanelActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
